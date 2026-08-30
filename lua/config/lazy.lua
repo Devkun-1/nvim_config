08887134655bb22f774fd6b8da2839f1f5a7e1a6
Devkun-1 <kun23985@gmail.com>
2026-08-30 17:05:11 +0700

    colorscheme && gitsign

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -2,7 +2,6 @@ require("config.options") -- options
 require("config.keymaps") -- keymaps
 require("config.autocmd") -- autocmd
 
-
 -- icons
 local icons = require("config.icons")
 
@@ -34,8 +33,8 @@ require("lazy").setup({
 })
 
 -- vim.cmd("colorscheme solarized-osaka")
-vim.cmd("colorscheme catppuccin")
--- vim.cmd("colorscheme tokyonight")
+-- vim.cmd("colorscheme catppuccin")
+vim.cmd("colorscheme tokyonight-night")
 -- vim.cmd("colorscheme ansi")
 -- vim.cmd("colorscheme zen")
 -- vim.cmd("colorscheme onedarker")
